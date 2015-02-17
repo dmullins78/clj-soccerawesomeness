196c5644a2b8512692498233cbe6c6a168ae9ee8
Dan Mullins <dmullins78@gmail.com>
2015-02-16 21:29:46 -0600

    sort teams by points

--- src/cljawesome/league/routes/league_routes.clj
+++ src/cljawesome/league/routes/league_routes.clj
@@ -1,23 +1,28 @@
 (ns cljawesome.league.routes.league-routes
   (:require [compojure.core :refer :all]
             [ring.util.response :refer [resource-response response]]
+            [selmer.parser :refer [render-file]]
+            [cljawesome.util.league-params :as lp]
             [cljawesome.league.models.query-defs :as query]))
 
-(defn league-params [request]
-  [(get-in request [:params :league-name])
-   (Integer. (get-in request [:params :year]))
-   (get-in request [:params :season])])
+(defn get-league [league year season]
+  (let [league (query/select-league-season {:name league :year year :season season})]
+    (response (first league) )))
 
-(defn get-league [request]
-  (let [[league year season] (league-params request)]
-    (let [league (query/select-league-season {:name league :year year :season season})]
-      (response (first league) ))))
+(defn get-divisions [team]
+  (:division team))
 
-(defn get-teams [request]
-  (let [[league year season] (league-params request)]
-    (let [teams (query/get-team-standings league year season)]
-      (response teams ))))
+(defn soccer-scorer
+  [t1 t2]
+  (if (> (:points t1) (:points t2))
+    true
+    false))
+
+(defn get-teams [league year season]
+  (let [teams (query/get-team-standings league (Integer. year) season)
+        sorted-teams (sort (comp soccer-scorer) teams)
+        sorted-divisions (group-by :division sorted-teams)]
+    (render-file "league.html" {:teams sorted-divisions :base (lp/basepath league year season)})))
 
 (defroutes league-routes
-  (GET  "/league/:league-name/:year/:season" [] get-league)
-  (GET  "/league/:league-name/:year/:season/teams" [] get-teams))
+  (GET  "/:league/:year/:season" [league year season] (get-teams league year season)))
