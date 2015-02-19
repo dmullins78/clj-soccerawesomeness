63c630d0062dc9edfbec9cacdc80b99d66fbbf42
Dan Mullins <dmullins78@gmail.com>
2015-02-18 22:49:39 -0600

    standardize on one jdbc url location

--- src/cljawesome/migrations/migration.clj
+++ src/cljawesome/migrations/migration.clj
@@ -1,19 +1,22 @@
 (ns cljawesome.migrations.migration
   (import [org.flywaydb.core Flyway])
-  (require [clojure.java.jdbc :refer [db-do-commands]]))
+  (require [clojure.java.jdbc :refer [db-do-commands]]
+           [clojure.string :as str]
+           [environ.core :refer [env]]))
 
 
 (defn get-db-datasource []
+  (let [url (java.net.URI. (env :database-url))
+        params (str/split (.getUserInfo url ) #":")]
   (doto (org.postgresql.ds.PGSimpleDataSource.)
-    (.setServerName "localhost")
-    (.setDatabaseName "cljawesome")
-    (.setUser "clj2")
-    (.setPassword "password")))
+    (.setServerName (.getHost url))
+    (.setDatabaseName (str/replace (.getPath url) #"/" ""))
+    (.setUser (first params))
+    (.setPassword (second params)))))
 
 (defn migrate []
-  (print "migrating 2 "))
   (let [datasource (get-db-datasource)
         flyway (doto (Flyway.)
                  (.setDataSource datasource)
                  (.setSqlMigrationPrefix ""))]
-    (.migrate flyway))
+    (.migrate flyway)))
