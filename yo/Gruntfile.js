095948ae37d9211024b22e16b7c48570d71c75cd
Dan Mullins <dmullins78@gmail.com>
2015-02-18 22:55:48 -0600

    versioning generated resources

--- yo/Gruntfile.js
+++ yo/Gruntfile.js
@@ -80,5 +80,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jst');
 
-  grunt.registerTask('default', ['sass','watch']);
+  grunt.registerTask('default', ['sass','jst','concat']);
 }
